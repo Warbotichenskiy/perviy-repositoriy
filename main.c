4872677018c59bc87bd7d36df4423e9e4670dcc5
Warbot <ytruttse@gmail.com>
2023-06-20 16:22:18 +0300

    izmeneniya

--- main.c
+++ main.c
@@ -3,6 +3,11 @@
 
 int main()
 {
-    printf("Hello world and all universe!\n");
+    printf("Dsoldier - armed with a plasma rifle \n");
+    printf("Height: 1.8 meters\n");
+    printf("Shoulder width: 80cm \n");
+    printf("Armor thickness: 4mm \n");
+    printf("Max. speed: 75km/h \n");
+    printf("Crace. speed: 60km/h\n");
     return 0;
 }
